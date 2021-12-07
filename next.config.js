61d95e668187aa3863d532a298e51a64ec26cbf1
Rudradeep Guha <rudradeep4@gmail.com>
2021-12-07 11:23:09 +0530

    env vars

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  env: {
-    MONGODB_URI: 'mongodb+srv://rudradeep4:newmonoultra@cluster0.ybgvh.mongodb.net/burningtrees?retryWrites=true&w=majority',
-    MONGODB_DB: 'burningtrees'
-  },
   reactStrictMode: true,
 }
