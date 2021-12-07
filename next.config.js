31615cf63e222f6a659d32a929295259321a8bfc
Rudradeep Guha <rudradeep4@gmail.com>
2021-12-07 11:11:45 +0530

    change next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  env: {
+    MONGODB_URI: 'mongodb+srv://rudradeep4:newmonoultra@cluster0.ybgvh.mongodb.net/burningtrees?retryWrites=true&w=majority',
+    MONGODB_DB: 'burningtrees'
+  },
   reactStrictMode: true,
 }
